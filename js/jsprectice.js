ed84701d66197dce2b2bc37255d4e1525e867acb
otsuburai <tapioka554@gmail.com>
2021-07-19 11:24:41 +0900

    portfolio

---
+++ js/jsprectice.js
@@ -0,0 +1,39 @@
+// document.write("Hello world!");
+// alert("Hello world!");
+
+// console.log("Hello world!" +"田中さん");
+// console.log(10+10);
+// console.log("10"+"10");
+
+// console.log(1+1);
+// console.log(1-1);
+// console.log(2*2);
+// console.log(10/3);
+// console.log(10%3);
+
+// let x = 1;
+// let y= 1;
+// x++;
+// console.log(x);
+// y--;
+// console.log(y);
+
+// let x = 7;
+// let y =x++;
+// let x2 = 7;
+// let y2 =++x2;
+// 変数とは　値を代入するための箱
+// 定数とは　値を別名で定着したもの
+
+// console.log(x);
+// console.log(y);
+// console.log(x2);
+// console.log(y2);
+'use strict';
+let age = 30;
+const name = "田中太郎";
+
+console.log(`${name}さんの年齢は${age}です`);
+
+// ES6　OR　ES2015
+// var　使わない
